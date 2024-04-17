2c4f51acfadd1144eaa9c92925ea04dbbf34b37b
Lucastnunez <lucastnunez@gmail.com>
2024-04-17 16:30:07 -0300

    Primer commit de prueba

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Assets, AssetsManifest, Sprite } from 'pixi.js'
 
 const app = new Application<HTMLCanvasElement>({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,35 @@ const app = new Application<HTMLCanvasElement>({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+export const manifest:AssetsManifest  = {
+    bundles: [
+        {
+            name : "AssetsPJ",
+            assets:
+            {
+                "Clampy the clamp" : "./clampy.png"
+            }
+        },
+    ]
+}
 
+async function init(): Promise<void> {
+    // Assets.init must only happen once! 
+    // Pack all your bundles into one manifest
+    await Assets.init({ manifest: manifest });
+
+    // Load the bundles you need
+    await Assets.loadBundle("AssetsPJ")
+    console.log("hola0")
+}
+
+console.log("hola1")
+const clampy= new Sprite(Assets.get("Clampy the clamp"))
+console.log("hola2")
 clampy.anchor.set(0.5);
-
+clampy.x
 clampy.x = app.screen.width / 2;
 clampy.y = app.screen.height / 2;
 
 app.stage.addChild(clampy);
+
