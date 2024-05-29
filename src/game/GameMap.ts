3ca9df61e5a34e584ca138c2c22845d4a85d0d86
Lucastnunez <lucastnunez@gmail.com>
2024-05-29 12:00:04 -0300

    Added interactive bushes generation.

--- src/game/GameMap.ts
+++ src/game/GameMap.ts
@@ -13,12 +13,14 @@ export class GameMap extends Container implements Updateable{
     public mapSprite: Sprite;
     //hitboxes
     //lights
-    public appleTrees:Array<InteractableObject>=new Array;
-    public bananaTrees:Array<InteractableObject>=new Array;
-    public lemonBushes:Array<InteractableObject>=new Array;
+    public appleTrees: Array<InteractableObject>=new Array;
+    public bananaTrees: Array<InteractableObject>=new Array;
+    public lemonBushes: Array<InteractableObject>=new Array;
     public grapeBushes: Array<InteractableObject>=new Array;
 
     public treePositions:ExtendedMap<Point,Boolean>;
+    public bushesPositions:ExtendedMap<Point,Boolean>;
+
     private interactingObject:InteractableObject|undefined = undefined;
     
     constructor(number:number){
@@ -40,7 +42,8 @@ export class GameMap extends Container implements Updateable{
         this.mapSprite.scale.set(1.5,1.5);
         this.addChild(this.mapSprite);
         this.treePositions=GameMisc.GetTreePositionsFromMap("map_1");
-        
+        this.bushesPositions=GameMisc.GetBushPositionsFromMap("map_1");
+
         // switch (this.mapNumber){
 
         // }
@@ -119,7 +122,7 @@ export class GameMap extends Container implements Updateable{
 
         let positions = this.treePositions.getKeys();
         let totalTrees:number=positions.length-1;
-        
+        console.log(positions,"trees");
 
         if (goals!=undefined){
 
@@ -129,9 +132,9 @@ export class GameMap extends Container implements Updateable{
             //Se calcula el minimo de arboles que deberia haber para poder completar los objetivos.
             if (goals.get("Bananas")){
                 minBanana=goals.get("Bananas")!/4;
-                if (minBanana>0 && minBanana<1) minBanana=1;
-                if (minBanana>1 && minBanana<2) minBanana=2;
-                if (minBanana>2 && minBanana<3) minBanana=3;
+                if (minBanana>0 && minBanana<=1) minBanana=1;
+                if (minBanana>1 && minBanana<=2) minBanana=2;
+                if (minBanana>2 && minBanana<=3) minBanana=3;
             }else{
                 minBanana=0;
             }
@@ -248,6 +251,146 @@ export class GameMap extends Container implements Updateable{
 
             
             this.addChild(tree);
+        }
+    }
+
+    public generateBushes(goals?:Map<String,number>){
+
+        let positions = this.bushesPositions.getKeys();
+        let totalBushes:number=positions.length-1;
+        console.log(positions,"bushes");
+
+        if (goals!=undefined){
+
+            let minGrapes:number;
+            let minLemons:number;
+
+            //Se calcula el minimo de arboles que deberia haber para poder completar los objetivos.
+            if (goals.get("Lemons")){
+                minLemons=goals.get("Lemons")!/4;
+                if (minLemons>0 && minLemons<=1) minLemons=1;
+                if (minLemons>1 && minLemons<=2) minLemons=2;
+                if (minLemons>2 && minLemons<=3) minLemons=3;
+                if (minLemons>3 && minLemons<=4) minLemons=4;
+                if (minLemons>4 && minLemons<=5) minLemons=5;
+            }else{
+                minLemons=0;
+            }
+
+            if (goals.get("Grapes")){
+                minGrapes=goals.get("Grapes")!/4;
+                if (minGrapes>0 && minGrapes<=1) minGrapes=1 ;
+                if (minGrapes>1 && minGrapes<=2) minGrapes=2 ;
+                if (minGrapes>2 && minGrapes<=3) minGrapes=3 ;
+                if (minGrapes>3 && minGrapes<=4) minGrapes=4 ;
+                if (minGrapes>4 && minGrapes<=5) minGrapes=5 ;
+            }else{
+                minGrapes=0
+            }
+            
+            
+
+            //Se crean los minimos arboles necesarios (de bananas)
+            for(let i=1;i<=minLemons;i++){
+                const lemonBush = new InteractableObject(Sprite.from("LemonBush"),"grab lemons");
+                let coord;
+                let random=0;
+
+                //Se obtiene una coord random de la lista de coords de arboles,
+                //y se controla que no esté ya ocupado por otro arbol.
+                do {                  
+                    random=GameMisc.RandomNumberInRange(0,(positions.length-1));
+                    coord = positions.at(random)!;
+                } while(this.bushesPositions.get(coord)==false);
+                
+                //La quita del array de posiciones para que no se vuelva a usar
+                positions.splice(random,1)
+
+                if(coord!=undefined) {
+                    lemonBush.position.set(coord.x,coord.y);
+                    this.bushesPositions.set(coord,false);
+                };
+                
+                //Lo agrega al array de arboles de bananas
+                this.lemonBushes.push(lemonBush);
+                this.addChild(lemonBush);
+                totalBushes-=1
+
+            }
+            //Se crean los minimos arboles necesarios (de manzanas)
+            for(let i=0;i<=minGrapes;i++){
+
+                const grapeBush = new InteractableObject(Sprite.from("GrapeBush"),"grab grapes");
+                
+                let random=0;
+                let coord;
+
+                //Se obtiene una coord random de la lista de coords de arboles,
+                //y se controla que no esté ya ocupado por otro arbol.
+                do{     
+                    random=GameMisc.RandomNumberInRange(0,(positions.length-1));
+                    coord = positions.at(random)!;
+                }while(this.bushesPositions.get(coord)==false)
+
+                //La quita del array de posiciones para que no se vuelva a usar
+                positions.splice(random,1)
+
+                if(coord!=undefined) {
+
+                    grapeBush.position.set(coord.x,coord.y);
+                    
+                    this.bushesPositions.set(coord,false);
+                };
+                
+                //Lo agrega al array de arboles de manzanas
+                this.appleTrees.push(grapeBush);
+                this.addChild(grapeBush);
+                
+                
+    
+                totalBushes-=1
+            }
+
+        }
+
+        for(let i=totalBushes; i>0; i--){
+
+            let random=GameMisc.RandomNumberInRange(1,3);
+            let coord;
+            let bush;
+
+            //Se selecciona un tipo de arbol al azar
+            switch(random){
+                case 2:
+                    bush=new InteractableObject(Sprite.from("GrapeBush"),"grab grapes");
+                    this.appleTrees.push(bush);
+                    break;
+                case 3:
+                    bush=new InteractableObject(Sprite.from("LemonBush"),"grab lemons");
+                    this.bananaTrees.push(bush);
+                    break; 
+                default:
+                    bush=Sprite.from("Bush");
+                    bush.anchor.set(0.5,0.5);
+                    break;
+            }
+            
+            //Selecciona una posicion libre al azar
+            do{     
+                random=GameMisc.RandomNumberInRange(0,(positions.length-1));
+                coord = positions.at(random)!;
+            }while(this.bushesPositions.get(coord)==false)
+
+            //Se elimina la posicion usada
+            positions.splice(random,1)
+
+            if(coord!=undefined) {
+                bush.position.set(coord.x,coord.y);
+                this.bushesPositions.set(coord,false);
+            };
+
+            
+            this.addChild(bush);
         }
     }
 }
